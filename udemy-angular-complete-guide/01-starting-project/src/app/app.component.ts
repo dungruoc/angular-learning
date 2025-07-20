e4f46b3847b7207df1d872076b975a160b551f2d
Minh-Dung Dang <minh-dungdang@Minh-Dungs-Mac-mini.local>
2025-07-20 17:51:17 +0700

    working with Input and Output in components

--- udemy-angular-complete-guide/01-starting-project/src/app/app.component.ts
+++ udemy-angular-complete-guide/01-starting-project/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId = this.users[0].id;
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+
+  get selectedUser() {
+    return this.users.find((user) => {
+      return user.id === this.selectedUserId;
+    });
+  }
+}
